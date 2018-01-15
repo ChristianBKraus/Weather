5af3a1750009723fd572f0e20b61f23f220ea3e5
D043312 <D043312@WDFN33414359A.emea.global.corp.sap>
2018-01-15 08:17:57 +0100

    API-Documentation by Swagger implemented

--- src/main/java/jupiterpa/controller/Controller.java
+++ src/main/java/jupiterpa/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.*;
 import jupiterpa.model.Daylight;
 import jupiterpa.model.Weather;
 import jupiterpa.service.LEDStripService;
@@ -14,6 +15,7 @@ import jupiterpa.service.WeatherService;
 
 @RequestMapping(path = Controller.PATH)
 @RestController
+@Api(value="weather", description="Weather Adapter")
 public class Controller {
     public static final String PATH ="/weather";
     
@@ -23,15 +25,28 @@ public class Controller {
     LEDStripService ledStripService;
     
     @GetMapping("")
+    @ApiOperation(value = "View todays Weather", response = Weather.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved Weather")
+    })
     public Weather get() {  
     	return weatherService.getWeather();
     }
+    
     @GetMapping("/daylight")
+    @ApiOperation(value = "View todays Sunrise and Sunset", response = Daylight.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved Daylight Hours")
+    })
     public Daylight getdaylight() {  
     	return weatherService.getDaylight();
     }
     
     @PutMapping("/update")
+    @ApiOperation(value = "Trigger an update of buffered information")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Updated Successfully")
+    })
     public void update() {
     	weatherService.update();
     	ledStripService.update(weatherService.getWeather(), weatherService.getDaylight());
